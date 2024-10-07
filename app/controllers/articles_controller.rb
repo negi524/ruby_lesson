ac4b618c60c742fd41f5c27b3b6a4fd301fab648
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-10-07 14:50:26 +0900

    feat: 記事を更新する

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,25 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def edit
+    # DBから記事を取得して、@articleに保存し、フォームを作成するときに使えるようにする
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    # DBから記事を取得
+    @article = Article.find(prams[:id])
+
+    # article_paramsでフィルタリングされた送信済みのデータで更新を試みる
+    if @article.update(article_params)
+      # 成功した場合は、記事ページに移動する
+      redirect_to @article
+    else
+      # 失敗した場合はeditをレンダリング
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :body)
