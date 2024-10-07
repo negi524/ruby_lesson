9067b8bfcab5cec405bd7a9068b775278b18507f
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-10-07 15:52:09 +0900

    feat: コメントの投稿機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+  def create
+    # 先に記事のオブジェクトを取得する
+    @article = Article.find(params[:article_id])
+    # コメントの作成と保存を同時に行っている
+    # コメントと記事が自動的にリンクされ、指定された記事に対してコメントが従属するようになる
+    @comment = @article.comments.create(comment_params)
+    # 元の記事の画面に戻る
+    redirect_to article_path(@article)
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:commenter, :body)
+  end
 end
