a16172ea9c9c2c9a2d1e71e8dbba2c9cbc58833e
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-10-07 15:27:06 +0900

    feat: Commentモデルの関連付けを行う

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :articles
+  resources :articles do
+    # articlesの内側にネストしたリソースとしてcommentsを作成
+    # 記事とコメントの関係性を階層的に捉える
+    resources :comments
+  end
 end
