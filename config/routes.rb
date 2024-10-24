568a4d33387ab50d6cb3b1132566950336479157
negi524 <egtnrumpbiizarre.info@gmail.com>
2024-10-24 21:33:29 +0900

    feat: メソッドを制限

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :articles do
+  resources :articles, only: [:show, :create] do
     # articlesの内側にネストしたリソースとしてcommentsを作成
     # 記事とコメントの関係性を階層的に捉える
     resources :comments
